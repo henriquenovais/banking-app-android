8e0b4466d90fbfa56857b10ff09b0f32c78bdfb7
Henrique <henriquennoliveira@gmail.com>
2022-09-26 17:07:11 -0300

    Implementing root app layer for future Firebase insertion

---
+++ app/src/main/java/com/example/bankapp/App.kt
@@ -0,0 +1,24 @@
+package com.example.bankapp
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import com.example.lifeinsuranceflow.di.lifeInsuranceFlowModule
+
+class App : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin{
+            androidContext(this@App)
+            modules(KOIN_MODULES)
+        }
+    }
+
+    companion object {
+        val KOIN_MODULES = listOf(
+            lifeInsuranceFlowModule
+        )
+    }
+}
---
+++ lifeinsuranceflow/src/main/java/com/example/lifeinsuranceflow/di/lifeInsuranceFlowModule.kt
@@ -0,0 +1,6 @@
+package com.example.lifeinsuranceflow.di
+
+import org.koin.dsl.module
+
+val lifeInsuranceFlowModule = module {
+}

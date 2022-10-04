37c32e22eb522583602c5928bdc3968f04de7dda
Henrique <henriquennoliveira@gmail.com>
2022-10-04 15:41:57 -0300

    Implement animated splash screen

--- homeflow/src/main/java/com/example/homeflow/activity/SplashActivity.kt
+++ homeflow/src/main/java/com/example/homeflow/activity/SplashActivity.kt
@@ -1,15 +1,28 @@
 package com.example.homeflow.activity
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import com.example.homeflow.R
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    val mainActivityIntent by lazy {
+        Intent(this, MainActivity::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
         supportActionBar?.hide()
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(mainActivityIntent)
+            finish()
+        }, 2000)
     }
 }

41f928e677d05857dc2abdd4079414499feb51e0
Henrique <henriquennoliveira@gmail.com>
2022-10-05 18:00:05 -0300

    Testing contrast options for toolbar

--- homeflow/src/main/java/com/example/homeflow/activity/HomeActivity.kt
+++ homeflow/src/main/java/com/example/homeflow/activity/HomeActivity.kt
@@ -2,12 +2,21 @@ package com.example.homeflow.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.example.homeflow.R
+import com.example.homeflow.fragment.HomeFragment
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setSupportActionBar(findViewById(R.id.my_toolbar))
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<HomeFragment>(R.id.fragment_container_home_flow)
+            }
+        }
     }
 }

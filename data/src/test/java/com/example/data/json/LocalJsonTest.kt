ce82bbef2c5a01f1d6ba8684ea5846fe4cfe7ac6
Henrique <henriquennoliveira@gmail.com>
2022-10-15 19:49:31 -0300

    trying to find ways to test json file opener

--- data/src/test/java/com/example/data/json/LocalJsonTest.kt
+++ data/src/test/java/com/example/data/json/LocalJsonTest.kt
@@ -1,15 +1,15 @@
 package com.example.data.json
 
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.spyk
 import io.mockk.verify
+import junit.framework.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
-import org.junit.internal.Classes.getClass
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
-import java.lang.Exception
-import java.nio.file.FileSystems
-import kotlin.coroutines.coroutineContext
 
 fun main(args: Array<String>) {
     val fakeJsonRelativePath = "/data/src/main/assets/test.json"
@@ -18,14 +18,22 @@ fun main(args: Array<String>) {
     val projectAbsolutePath = File("").absolutePath
     val testJsonAbsolutePath = projectAbsolutePath + fakeJsonRelativePath
     println(testJsonAbsolutePath)
+
+    val localJson = LocalJson()
+    val result = localJson.readJsonFileRecursive(testJsonAbsolutePath)
+    println(result)
 }
 
 class LocalJsonTest {
-    val localJson = LocalJson()
+    private val localJson = LocalJson()
 
-    val mBufferedReader by lazy {
-        BufferedReader(FileReader(fakeJsonAbsolutePath))
+    private val fakeJsonAbsolutePath by lazy {
+        projectAbsolutePath + FAKE_JSON_RELATIVE_PATH
     }
+
+    @MockK
+    val mBufferedReader = spyk(BufferedReader(FileReader(fakeJsonAbsolutePath)))
+
     /*
         val fakeJsonPath = getClass(this.toString()).classLoader!!.getResource("test.json").path
     */
@@ -39,10 +47,14 @@ class LocalJsonTest {
     @Test
     fun readJsonFile() {
         //Arrange
+        every { localJson.bufferedReader.readLine() } returns "this"
 
         //Act
+        val result = localJson.readJsonFile(fakeJsonAbsolutePath)
+        println(result)
 
         //Assert
+        assertTrue(result == FAKE_JSON_CONTENT)
     }
 
     @Test
@@ -55,6 +67,71 @@ class LocalJsonTest {
     companion object {
         private val projectAbsolutePath = File("").absolutePath
         private const val FAKE_JSON_RELATIVE_PATH = "/src/main/assets/test.json"
-        private val fakeJsonAbsolutePath = projectAbsolutePath + FAKE_JSON_RELATIVE_PATH
+
+        private const val FAKE_JSON_CONTENT = "{
+        "'operation_buttons": [
+        {
+            "title": "Área PIX",
+            "icon": "#$90812",
+            "background_color": "#FFFFFF",
+            "content_description": "[Botão]Toque para menu de transações PIX"
+        },
+        {
+            "title": "Pagar",
+            "icon": "#$90812",
+            "background_color": "#FFFFFF",
+            "description": "[Botão]Toque para pagamentos de boleto"
+        },
+        {
+            "title": "Pegar Emprestado",
+            "icon": "#$90812",
+            "background_color": "#FFFFFF",
+            "description": "[Botão]Toque para empréstimos"
+        },
+        {
+            "title": "Transferir",
+            "icon": "#$90812",
+            "background_color": "#FFFFFF",
+            "description": "[Botão]Toque para realizar uma transferência"
+        },
+        {
+            "title": "Depositar",
+            "icon": "#$90812",
+            "background_color": "#FFFFFF",
+            "description": "[Botão]Toque para realizar um depósito"
+        },
+        {
+            "title": "Recarga de celular",
+            "icon": "#$90812",
+            "background_color": "#FFFFFF",
+            "description": "[Botão]Toque para realizar recarga de celular"
+        },
+        {
+            "title": "Cobrar",
+            "icon": "#$90812",
+            "background_color": "#FFFFFF",
+            "description": "[Botão]Toque para gerar boletos"
+        },
+        {
+            "title": "Doação",
+            "icon": "#$90812",
+            "background_color": "#FFFFFF",
+            "description": "[Botão]Toque para realizar doação"
+        },
+        {
+            "title": "Transferir Internac.",
+            "icon": "#$90812",
+            "background_color": "#FFFFFF",
+            "description": "[Botão]Toque para realizar transferência internacional"
+        },
+        {
+            "title": "Investir",
+            "icon": "#$90812",
+            "background_color": "#FFFFFF",
+            "description": "[Botão]Toque para investir"
+        }
+        ]
+    }"
+
     }
 }
